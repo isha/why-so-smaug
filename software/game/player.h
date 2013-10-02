a51fa21c9fc40e6aadadd2d6b90d19b293fc1879
Rafael Jacinto <rpdjacinto@gmail.com>
2013-10-01 17:37:09 -0700

    Added initialization constants for the Player constructur

--- software/game/player.h
+++ software/game/player.h
@@ -2,6 +2,12 @@
 #define PLAYER_H_
 
 #define MAX_HEALTH 3
+#define START_TIME 0
+#define START_SCORE 0
+#define START_VELOCITY_X 0
+#define START_VELOCITY_Y 0
+#define START_COORDINATE_X 120
+#define START_COORDINATE_Y 0
 
 typedef struct{
 	char* screen_name;
@@ -15,15 +21,15 @@ typedef struct{
 	int coordinates_y;
 } Player;
 
-void construct_player(Player* player, char* screen_name, int score, int time, int health, int velocity_x, int velocity_y, int coordinates_x, int coordinates_y) {
+void construct_player(Player* player, char* screen_name) {
 	player->screen_name = screen_name;
-	player->score = score;
-	player->time = time;
-	player->health = health;
-	player->velocity_x = velocity_x;
-	player->velocity_y = velocity_y;
-	player->coordinates_x = coordinates_x;
-	player->coordinates_y = coordinates_y;
+	player->score = START_SCORE;
+	player->time = START_TIME;
+	player->health = MAX_HEALTH;
+	player->velocity_x = START_VELOCITY_X;
+	player->velocity_y = START_VELOCITY_Y;
+	player->coordinates_x = START_COORDINATE_X;
+	player->coordinates_y = START_COORDINATE_Y;
 }
 
 void set_coordinates(Player* player) {
