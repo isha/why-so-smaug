70dbedac9196a0c932dedd3b1d89f0eb35324341
joeycarlos <jclaguio@gmail.com>
2013-10-07 21:35:09 -0700

    Added collision detection and collision test

--- software/game/main.c
+++ software/game/main.c
@@ -14,6 +14,8 @@
 #define keys (volatile char *) BUTTONS_BASE
 #define debounce_interval 250000
 
+#define COLLISION_DAMAGE 10
+
 bool buttons[4] = {false, false, false, false};
 // 0 is not pressed
 
@@ -38,23 +40,43 @@ int main(void)
 	Player *player1 = construct_player(name);
 	printf("\nPlayer created with name %s", player1->screen_name);
 
-//	Map *map = construct_map();
-//	printf("\nConstructed Map with velocity %d", map->velocity);
+	Map *map = construct_map();
+	printf("\nConstructed Map with velocity %d", map->velocity);
+
+	test_collision(player1, map);
 
 	printf("\nRock n' Roll\n");
-	while(1) {
+/*
+	while(player1->health > 0) {
 		read_buttons();
 		new_timestamp = alt_timestamp();
-		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
-		alt_up_pixel_buffer_dma_draw(pixel_buffer, 0x00, player1->coordinates_x, player1->coordinates_y);
-		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
-		if(new_timestamp >= old_timestamp + debounce_interval) {
-			move_player(player1);
-			old_timestamp = new_timestamp;
-		}
-		alt_up_pixel_buffer_dma_draw(pixel_buffer, 0x740, player1->coordinates_x, player1->coordinates_y);
-		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
+//		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
+//		alt_up_pixel_buffer_dma_draw(pixel_buffer, 0x00, player1->coordinates_x, player1->coordinates_y);
+//		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
+//		if(new_timestamp >= old_timestamp + debounce_interval) {
+//			move_player(player1);
+//			old_timestamp = new_timestamp;
+//		}
+//		alt_up_pixel_buffer_dma_draw(pixel_buffer, 0x740, player1->coordinates_x, player1->coordinates_y);
+//		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
+
+		check_collision(player1, map);
+
+		// Calculations
+		next_map(map);
+
+		// Modify screen buffer matrix
+		update_screen(map);
+
+		// Character buffer
+		text(map, player1);
+
+		// Draw to screen
+		draw_to_screen();
+
 	}
+*/
+
 //	while (player1->health != 0 && game_on) {
 //		// Calculations
 //		next_map(map);
@@ -76,6 +98,74 @@ int main(void)
 	game_over();
 
 	return 0;
+}
+
+void check_collision(Player* player, Map* map) {
+
+	// point at the first obstacle
+	Obstacle * current_obstacle = map->obstacles;
+
+	// check if coordinates of player are same as obstacles on map for all obstacles
+	while(current_obstacle != NULL) {
+		printf("\nChecking linked list...");
+		if (player->coordinates_x == current_obstacle->coordinates_x && player->coordinates_y == current_obstacle->coordinates_y) {
+			on_collide(player, current_obstacle->type);
+			printf("\nCollision has occurred!");
+			return; // if the coordinates of player are same as obstacle, collision occurred
+		}
+		current_obstacle = current_obstacle->next; // start looking at the next obstacle
+	}
+
+	return;	// no collisions were detected
+}
+
+void on_collide(Player* player, ObstacleType type ) {
+	switch(type) {
+	case WALL:
+		damage_health(player, 10);
+		break;
+	case POTION:
+		set_health(player, MAX_HEALTH);
+		break;
+	case WENCH:
+		damage_health(player, 50);
+		break;
+	case CHEST:
+		add_score(player, 100);
+		break;
+	case COIN:
+		add_score(player, 10);
+		break;
+	case POISON:
+		damage_health(player, 50);
+		break;
+	default:
+		// do nothing
+		break;
+	}
+}
+
+void damage_health(Player* player, int damage) {
+	player->health = player->health - damage;
+	printf ("\nPlayer has been damaged %d health", damage);
+}
+
+void set_health(Player* player, int new_health) {
+	player-> health = new_health;
+	printf ("\nYour health has returned to %d", new_health);
+}
+
+void add_score(Player* player, int score) {
+	player->score = player->score + score;
+	printf ("\nYou gained %d points", score);
+}
+
+void test_collision(Player* player, Map* map) {
+	player->coordinates_x = 100;
+	player->coordinates_y = 240;
+
+	check_collision(player, map);
+
 }
 
 void read_buttons() {
